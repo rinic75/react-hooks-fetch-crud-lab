44f654559bf770770b435fe06897f0c9766c2fff
Chang Shin <cshin7374@gmail.com>
2022-07-29 00:42:26 -0500

    commit tree 289a7f22a45b6cd3a6cdf3eabbcaadaa54ce5170

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDelete, handleUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleClick() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+    .then(() => handleDelete(id))
+  }
+
+  function handleChange(e) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex : parseInt(e.target.value)
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedQ) => handleUpdate(updatedQ));
+  }
+  
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +38,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+  
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions")
+    .then(r=>r.json())
+    .then(questions => setQuestions(questions))
+  },[])
+
+  function handleDelete(deletedId) {
+    const updatedQuestions = questions.filter(question => question.id !== deletedId)
+    setQuestions(updatedQuestions)
+  }
+
+  function handleUpdate(updatedQuestion) {
+    const updatedQuestions = questions.map(question => {
+      if(question.id === updatedQuestion.id) {
+        return updatedQuestion
+      } else {
+        return question
+      }
+    })
+    setQuestions(updatedQuestions)
+  }
+
+  const questionsItem = questions.map(quest => {
+    return <QuestionItem 
+            key={quest.id} 
+            question={quest}
+            handleDelete={handleDelete}
+            handleUpdate={handleUpdate} />
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionsItem}</ul>
     </section>
   );
 }
